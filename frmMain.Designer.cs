d203d694aac1fc8cb573a01268889dfaae74851b
dunkan50 <dunkim2008@gmail.com>
2016-05-20 19:37:42 +0300

    first commit

---
+++ frmMain.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace stitching2
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tblOuter = new System.Windows.Forms.TableLayoutPanel();
+            this.btnOpenFile = new System.Windows.Forms.Button();
+            this.btSaveImage = new System.Windows.Forms.Button();
+            this.ibFinalImage = new Emgu.CV.UI.ImageBox();
+            this.dgvSourceImage = new System.Windows.Forms.DataGridView();
+            this.ofdOpenfile = new System.Windows.Forms.OpenFileDialog();
+            this.sfdSaving = new System.Windows.Forms.SaveFileDialog();
+            this.samplePictureColumn = new System.Windows.Forms.DataGridViewImageColumn();
+            this.fileNameColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tblOuter.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ibFinalImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSourceImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tblOuter
+            // 
+            this.tblOuter.ColumnCount = 2;
+            this.tblOuter.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tblOuter.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tblOuter.Controls.Add(this.btnOpenFile, 0, 0);
+            this.tblOuter.Controls.Add(this.btSaveImage, 1, 0);
+            this.tblOuter.Controls.Add(this.ibFinalImage, 1, 1);
+            this.tblOuter.Controls.Add(this.dgvSourceImage, 0, 1);
+            this.tblOuter.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tblOuter.Location = new System.Drawing.Point(0, 0);
+            this.tblOuter.Name = "tblOuter";
+            this.tblOuter.RowCount = 2;
+            this.tblOuter.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tblOuter.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tblOuter.Size = new System.Drawing.Size(849, 515);
+            this.tblOuter.TabIndex = 0;
+            // 
+            // btnOpenFile
+            // 
+            this.btnOpenFile.Location = new System.Drawing.Point(3, 3);
+            this.btnOpenFile.Name = "btnOpenFile";
+            this.btnOpenFile.Size = new System.Drawing.Size(75, 23);
+            this.btnOpenFile.TabIndex = 0;
+            this.btnOpenFile.Text = "Open File";
+            this.btnOpenFile.UseVisualStyleBackColor = true;
+            this.btnOpenFile.Click += new System.EventHandler(this.btnOpenFile_Click);
+            // 
+            // btSaveImage
+            // 
+            this.btSaveImage.Location = new System.Drawing.Point(427, 3);
+            this.btSaveImage.Name = "btSaveImage";
+            this.btSaveImage.Size = new System.Drawing.Size(75, 23);
+            this.btSaveImage.TabIndex = 1;
+            this.btSaveImage.Text = " Save ";
+            this.btSaveImage.UseVisualStyleBackColor = true;
+            this.btSaveImage.Click += new System.EventHandler(this.btSaveImage_Click);
+            // 
+            // ibFinalImage
+            // 
+            this.ibFinalImage.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.ibFinalImage.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ibFinalImage.Location = new System.Drawing.Point(427, 32);
+            this.ibFinalImage.Name = "ibFinalImage";
+            this.ibFinalImage.Size = new System.Drawing.Size(419, 480);
+            this.ibFinalImage.TabIndex = 2;
+            this.ibFinalImage.TabStop = false;
+            // 
+            // dgvSourceImage
+            // 
+            this.dgvSourceImage.AllowUserToAddRows = false;
+            this.dgvSourceImage.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dgvSourceImage.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvSourceImage.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.samplePictureColumn,
+            this.fileNameColumn});
+            this.dgvSourceImage.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvSourceImage.Location = new System.Drawing.Point(3, 32);
+            this.dgvSourceImage.Name = "dgvSourceImage";
+            this.dgvSourceImage.ReadOnly = true;
+            this.dgvSourceImage.Size = new System.Drawing.Size(418, 480);
+            this.dgvSourceImage.TabIndex = 3;
+            // 
+            // ofdOpenfile
+            // 
+            this.ofdOpenfile.FileName = "openFileDialog1";
+            // 
+            // samplePictureColumn
+            // 
+            this.samplePictureColumn.HeaderText = "Sample Picture";
+            this.samplePictureColumn.Name = "samplePictureColumn";
+            this.samplePictureColumn.ReadOnly = true;
+            this.samplePictureColumn.FillWeight = 200F;
+            this.samplePictureColumn.Width = 200;
+
+            // 
+            // fileNameColumn
+            // 
+            this.fileNameColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.fileNameColumn.HeaderText = "FIle Name";
+            this.fileNameColumn.Name = "fileNameColumn";
+            this.fileNameColumn.ReadOnly = true;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(849, 515);
+            this.Controls.Add(this.tblOuter);
+            this.Name = "frmMain";
+            this.Text = "Form1";
+            this.tblOuter.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.ibFinalImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSourceImage)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tblOuter;
+        private System.Windows.Forms.Button btnOpenFile;
+        private System.Windows.Forms.Button btSaveImage;
+        private Emgu.CV.UI.ImageBox ibFinalImage;
+        private System.Windows.Forms.DataGridView dgvSourceImage;
+        private System.Windows.Forms.DataGridViewImageColumn samplePictureColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn fileNameColumn;
+        private System.Windows.Forms.OpenFileDialog ofdOpenfile;
+        private System.Windows.Forms.SaveFileDialog sfdSaving;
+    }
+}
+
---
+++ frmMain.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+using Emgu.CV;
+using Emgu.CV.Util;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using Emgu.CV.Stitching;
+
+
+////////////////////////////////////////////////////////////////////////////////////////////
+
+namespace stitching2
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+        
+        ////////////////////////////////////////////////////////////////////////////////////////////
+       
+        private void btnOpenFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofdOpenfilesD = new OpenFileDialog();
+
+            ofdOpenfilesD.CheckFileExists = true;
+            ofdOpenfilesD.Multiselect = true;
+
+            if(ofdOpenfilesD .ShowDialog()!=DialogResult.OK||ofdOpenfilesD.FileName=="")
+            {
+                MessageBox.Show("Can not read image");
+
+            }
+            else if(ofdOpenfilesD.ShowDialog()==DialogResult.OK)
+            {
+                dgvSourceImage.Rows.Clear();
+               
+
+                Image<Bgr, Byte>[] originalImagesU = new Image<Bgr, byte>[ofdOpenfilesD.FileNames.Length];
+
+                for (int i=0;i<originalImagesU.Length;i++)
+                {
+                    originalImagesU[i] = new Image<Bgr, byte>(ofdOpenfilesD.FileNames[i]);
+
+                    using (Image<Bgr, byte> firstImage = originalImagesU[i].Resize(200, 200, Inter.Cubic, true)) 
+
+                    {
+                        DataGridViewRow row = dgvSourceImage.Rows[dgvSourceImage.Rows.Add()];
+                        row.Cells["fileNameColumn"].Value = ofdOpenfilesD.FileNames[i];
+                        row.Cells["samplePictureColumn"].Value = firstImage.ToBitmap();
+                        row.Height = 200;
+
+                        
+                    }
+                }
+                try
+                {
+                    using (Stitcher stiching = new Stitcher(false))
+                    {
+                        using (VectorOfMat matVector = new VectorOfMat())
+                        {
+                            Mat finalImageN = new Mat();
+                            matVector.Push(originalImagesU);
+                            stiching.Stitch(matVector, finalImageN);
+                            ibFinalImage.Image = finalImageN;
+
+                        }
+                    }
+                }
+                finally
+                {
+                    foreach(Image<Bgr,Byte> image in originalImagesU)
+                    {
+                        image.Dispose();
+                    }
+                }
+            }
+
+        }
+        ////////////////////////////////////////////////////////////////////////////////////////////
+
+
+        private void btSaveImage_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog svdImages = new SaveFileDialog();
+
+            svdImages.Title = "save images.";
+
+            svdImages.ShowDialog();
+            /*
+            if(svdImages.FileName!="")
+            {
+                FileStream fsSave = (FileStream)svdImages.OpenFile();
+
+                this.btSaveImage.Image.Save(fsSave, System.Drawing.Imaging.ImageFormat.Jpeg);
+            }
+            */
+        }
+        ////////////////////////////////////////////////////////////////////////////////////////////
+
+    }
+}
